f73aa356d769031756c9c4d661ddea9a2be3751a
gzc <a1429257921@163.com>
2023-06-11 04:38:46 +0800

    完善启动类、配置文件和日志文件

--- jason-modules/jason-photography-api/src/main/java/com/jason/photography/api/JasonPhotographyApiApplication.java
+++ jason-modules/jason-photography-api/src/main/java/com/jason/photography/api/JasonPhotographyApiApplication.java
@@ -1,6 +1,7 @@
 package com.jason.photography.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,6 +15,7 @@ import org.springframework.util.StringUtils;
  */
 @Slf4j
 @SpringBootApplication
+@MapperScan("com.jason.photography.dao.mapper")
 public class JasonPhotographyApiApplication {
 
     public static void main(String[] args) {
@@ -30,16 +32,17 @@ public class JasonPhotographyApiApplication {
         // 获取当前部署的环境
         String[] activeProfiles = applicationContext.getEnvironment().getActiveProfiles();
         log.info("部署环境->{}", StringUtils.arrayToCommaDelimitedString(activeProfiles));
-        System.out.println("(♥◠‿◠)ﾉﾞ  坤坤博客系统启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " _                      _     _             \n" +
-                "| |                    | |   | |            \n" +
-                "| | ___   _ _ __ ______| |__ | | ___   __ _ \n" +
-                "| |/ / | | | '_ \\______| '_ \\| |/ _ \\ / _` |\n" +
-                "|   <| |_| | | | |     | |_) | | (_) | (_| |\n" +
-                "|_|\\_\\\\__,_|_| |_|     |_.__/|_|\\___/ \\__, |\n" +
-                "                                       __/ |\n" +
-                "                                      |___/ \n" +
-                "\n"
+        System.out.println("""
+                (♥◠‿◠)ﾉﾞ  jason摄影展用户端启动成功   ლ(´ڡ`ლ)ﾞ \s
+                   _                                   _           _                              _                             _\s
+                  (_)                                 | |         | |                            | |                           (_)
+                   _  __ _ ___  ___  _ __ ______ _ __ | |__   ___ | |_ ___   __ _ _ __ __ _ _ __ | |__  _   _ ______ __ _ _ __  _\s
+                  | |/ _` / __|/ _ \\| '_ \\______| '_ \\| '_ \\ / _ \\| __/ _ \\ / _` | '__/ _` | '_ \\| '_ \\| | | |______/ _` | '_ \\| |
+                  | | (_| \\__ \\ (_) | | | |     | |_) | | | | (_) | || (_) | (_| | | | (_| | |_) | | | | |_| |     | (_| | |_) | |
+                  | |\\__,_|___/\\___/|_| |_|     | .__/|_| |_|\\___/ \\__\\___/ \\__, |_|  \\__,_| .__/|_| |_|\\__, |      \\__,_| .__/|_|
+                 _/ |                           | |                          __/ |         | |           __/ |           | |     \s
+                |__/                            |_|                         |___/          |_|          |___/            |_|     \s
+                """
         );
     }
 
